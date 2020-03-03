5b7afcc3c0174e8e6ace188ac7218484de93dfd2
Jasmin Soltani <jksoltani@gmail.com>
2020-03-03 14:50:37 -0500

    merging w Malinda

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 module.exports = router
 
+//ROUTES: /api/users
 router.use('/users', require('./users'))
+
+//ROUTES: /api/fruit
+router.use('/fruit', require('./fruit'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
