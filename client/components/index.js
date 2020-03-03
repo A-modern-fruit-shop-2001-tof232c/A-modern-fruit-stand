fdf9291128bd1c291da30374bf54c6c31739b8f9
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-03-03 17:57:43 -0500

    working on addcartbutton

--- client/components/index.js
+++ client/components/index.js
@@ -6,4 +6,5 @@
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
 export {Login, Signup} from './auth-form'
-export {fruitSingle} from './fruit-single'
+export {default as FruitSingle} from './fruit-single'
+export {default as ButtonAddToCart} from './button-add-to-cart'
