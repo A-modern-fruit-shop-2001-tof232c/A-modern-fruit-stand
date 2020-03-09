23f9623fab1dc006d6785464eb928cf55e4f0ae4
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-03-09 16:00:57 -0400

    fix: util function pennies to decimal

--- util/util-deciInt.js
+++ util/util-deciInt.js
@@ -1,7 +1,7 @@
 // convert price in pennies to decimal number like: 1.00
 
 const intToDecimal = function(penniesNumber) {
-  return penniesNumber / 100
+  return (penniesNumber / 100).toFix(2)
 }
 
 module.exports = intToDecimal
